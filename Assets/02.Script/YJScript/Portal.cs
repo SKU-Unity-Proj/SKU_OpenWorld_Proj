1e06aba18851c47344a868d2e130feae861a8761
20190965YoungJin <dudwls1564954@naver.com>
2023-10-12 16:43:15 +0900

    0

--- Assets/02.Script/YJScript/Portal.cs
+++ Assets/02.Script/YJScript/Portal.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
     void OnTriggerEnter(Collider col)
     {
-        SceneManager.LoadScene("3");
+        col.transform.position = new Vector3(224f, 0f, 0f);
     }
 }
