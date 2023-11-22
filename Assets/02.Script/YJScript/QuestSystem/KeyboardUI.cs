fd3d177ef90cd43e1aae3a4c81304f5689780af7
20190965YoungJin <dudwls1564954@naver.com>
2023-11-22 20:54:03 +0900

    1

--- Assets/02.Script/YJScript/QuestSystem/KeyboardUI.cs
+++ Assets/02.Script/YJScript/QuestSystem/KeyboardUI.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class KeyboardUI : MonoBehaviour
+public class KeyboardGuid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI textMesh;
+    public float fadeTime = 2f;
+
+    void OnTriggerEnter(Collider col)
     {
-        
+        if(col.tag == "Player")
+            StartCoroutine(FadeFullAlpha());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FadeFullAlpha() // 알파값 0에서 1로 전환
     {
-        
+        textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, 0);
+        while (textMesh.color.a < 1.0f)
+        {
+            textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, textMesh.color.a + (Time.deltaTime / fadeTime));
+            yield return null;
+        }
+        StartCoroutine(FadeZero());
+    }
+
+    IEnumerator FadeZero()  // 알파값 1에서 0으로 전환
+    {
+        textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, 1);
+        while (textMesh.color.a > 0.0f)
+        {
+            textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, textMesh.color.a - (Time.deltaTime / fadeTime));
+            yield return null;
+        }
     }
 }
